a60c992a3768cc7bf04fd8be0b0b3424a7c77375
halgham2 <Hesham@192.168.0.6>
2017-02-20 23:01:55 -0700

    change in savings descreption

--- ser316labcr/given/src/banking/primitive/core/Savings.java
+++ ser316labcr/given/src/banking/primitive/core/Savings.java
@@ -1,3 +1,6 @@
+//child object of an account that is more specific to be savings is used to show the customer account and it's information
+
+
 package banking.primitive.core;
 
 public class Savings extends Account {
